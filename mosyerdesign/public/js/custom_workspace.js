35abde39805847913f776dbc10770c558c60daaf
tahir-zaqout <zaqout2000@gmail.com>
2022-04-04 16:18:45 +0300

    Update Fonts

--- mosyerdesign/public/js/custom_workspace.js
+++ mosyerdesign/public/js/custom_workspace.js
@@ -82,16 +82,14 @@ frappe.views.Workspace.prototype.build_sidebar_section = function (title, items)
 					<span class="sidebar-item-label">${__(item.label) || __(item.name)}<span>
 				</a>
 				<div style="padding-right:5px; margin-left: 10px;">
-				${item.items.length > 0 ?
-					`<button class="btn btn-default btn-sm dropdown-btn" data-toggle="dropdown" aria-expanded="false" data-original-title="Menu"
+					<button class="btn btn-default btn-sm dropdown-btn" data-toggle="dropdown" aria-expanded="false" data-original-title="Menu"
 								style="box-shadow: none !important;">
 								<svg class="icon icon-xs"><use href="#icon-small-down"></use></svg>
-					</button>`
-					:''}
+					</button>
 					<ul class="dropdown-menu dropdown-menu-right" style="overflow-x: hidden; max-height:450px"> 
 						${item.items.map(el=>
 							`<li> 
-								<a href="/app/${frappe.router.slug(item.name)}/${el}" class="dropdown-item">${__(el)}</a>
+								<a href="/app/${frappe.router.slug(item.name)}/${el}" class="dropdown-item" style="font-size:14px">${__(el)}</a>
 							</li>`).join('')} 
 					</ul>
 				</div>	
