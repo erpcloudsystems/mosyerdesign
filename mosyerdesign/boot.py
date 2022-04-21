5725f0de0dfecd9cbdd6a03616072cdbd884d244
kamalghanima <kamalghanima123@gmail.com>
2022-04-21 17:03:12 +0300

    Add a section for employee in system controller

--- mosyerdesign/boot.py
+++ mosyerdesign/boot.py
@@ -11,21 +11,39 @@ def boot_session(bootinfo):
 def get_sidebar_items():
     system_controller = frappe.get_single('System Controller')
     labels = []
-    if len(system_controller.sidebar_labels):
-        for row in system_controller.sidebar_labels:
-            labels.append({'label': row.label, 'name': row.label,'icon': row.icon, 'child_items':[]})
+    if "Employee" in frappe.get_roles(frappe.session.user):
+        if len(system_controller.employee_label):
+            for row in system_controller.employee_label:
+                labels.append({'label': row.label, 'name': row.label,'icon': row.icon, 'child_items':[]})
 
-        for label in labels:
-            for row in system_controller.sidebar_item:
-                route = ''
-                if row.type == 'Report':
-                    route = 'query-report/' + row.doc_name
-                elif row.type == 'DocType':
-                    route = '-'.join(row.doc_name.lower().split(' '))
+            for label in labels:
+                for row in system_controller.employee_shortcuts:
+                    route = ''
+                    if row.type == 'Report':
+                        route = 'query-report/' + row.doc_name
+                    elif row.type == 'DocType':
+                        route = '-'.join(row.doc_name.lower().split(' '))
 
-                if label.get('label') == row.parent_name:
-                    label.get('child_items').append({'route':route, 'name':row.doc_name, 'label':row.label, 'icon':row.icon, 'route':route})
-    return labels
+                    if label.get('label') == row.parent_name:
+                        label.get('child_items').append({'name':row.doc_name, 'label':row.label, 'icon':row.icon, 'route':route})    
+        return labels
+
+    else:
+        if len(system_controller.sidebar_labels):
+            for row in system_controller.sidebar_labels:
+                labels.append({'label': row.label, 'name': row.label,'icon': row.icon, 'child_items':[]})
+
+            for label in labels:
+                for row in system_controller.sidebar_item:
+                    route = ''
+                    if row.type == 'Report':
+                        route = 'query-report/' + row.doc_name
+                    elif row.type == 'DocType':
+                        route = '-'.join(row.doc_name.lower().split(' '))
+
+                    if label.get('label') == row.parent_name:
+                        label.get('child_items').append({'name':row.doc_name, 'label':row.label, 'icon':row.icon, 'route':route})
+        return labels
 
 def get_doctypes_notification():
     system_controller = frappe.get_single('System Controller')
@@ -40,3 +58,26 @@ def get_doctypes_notification():
 def enable_system_controller():
     system_controller = frappe.get_single('System Controller')
     return system_controller.enable
+
+
+# def get_doctypes_employee():
+#     # system_controller = frappe.get_single('System Controller')
+#     # docs_info = []
+#     # if len(system_controller.employee_shortcuts):
+#     #     for doc in system_controller.employee_shortcuts:
+#     #         if frappe.has_permission(doctype=doc.title, user=frappe.session.user):
+#     #             lst =  frappe.get_list(doc.title, fields=['*'], filters={'docstatus':1}, limit=5)
+#     #             docs_info += lst
+#     # return docs_info
+
+#     system_controller = frappe.get_single('System Controller')
+#     labels = []
+#     if len(system_controller.employee_shortcuts):
+#         for row in system_controller.employee_shortcuts:
+#             labels.append({'title': row.title,'icon': row.icon, 'child_items':[]})
+
+#         for label in labels:
+#             for row in system_controller.employee_shortcuts:
+#                 if frappe.has_permission(doctype=row.title, user=frappe.session.user):
+#                     label.get('child_items').append({'icon':row.icon, 'title':row.title})
+#     return labels
